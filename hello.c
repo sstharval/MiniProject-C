144059c781072e5c6944dd4292c664dc0faaf311
Isha Prabhu <67497698+IshaPrabhu-260025@users.noreply.github.com>
2021-04-08 18:05:07 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    printf("Welcome to Github Actions.\n********* Its Working*******\n");
+    printf("Welcome to Github Actions.\n********* Its Working**\n");
     printf("Hurry");
     return 0;
 }
