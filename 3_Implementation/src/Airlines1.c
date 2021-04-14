e98e6816850f776bd9b583b9c4738877240cd103
Isha Prabhu <67497698+IshaPrabhu-260025@users.noreply.github.com>
2021-04-14 11:11:25 +0530

    Update Airlines1.c

--- 3_Implementation/src/Airlines1.c
+++ 3_Implementation/src/Airlines1.c
@@ -2,7 +2,7 @@
 #include"../inc/Airlines.h"
 struct IndiGo_airline
 {
-	char passport[6];
+	char passport[10];
 	char name[15];
     char destination[15];
 	int seat_num;
@@ -16,8 +16,8 @@ struct IndiGo_airline
 
 void details()
 {
-	printf("\n\t Enter your passport number:");
-	fgets(stream->passport,6,stdin);    //reads a line from stdin and stores it into the string pointed
+	printf("\n\t Enter your 6 digit passport number:");
+	fgets(stream->passport,10,stdin);    //reads a line from stdin and stores it into the string pointed
 	printf("\n\t Enter your  name:");
 	fgets(stream->name,15,stdin); 
 	printf("\n\t Enter your email address:");
